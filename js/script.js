8f5b3764688fc1a7c2da4713ec5df4810edaa2b8
frontend-antony <antony@vivevacationsoutlet.com>
2022-06-04 00:02:11 -0500

    subiendo nueva web

--- js/script.js
+++ js/script.js
@@ -1,62 +1,12 @@
 $(document).ready(function() {
-    // navbar active
-    navbarActive();
-    function navbarActive() {
-        var scrollLink = $('.navbar ul li a[href*="#"]');
-        var scrollLinkUx = $(".nav_ux");
+	$(".js__popup-login").on("click", function() {
+		$(".popup__login").addClass("popup_animation");
+		$(".page-body").css({"overflow": "hidden"});
+		// $(".popup__login").css({"display": "block"});
+	});
 
-        // smooth scrolling
-        scrollLink.click(function(e) {
-            e.preventDefault();
-            $("html, body").animate({
-                scrollTop: $(this.hash).offset().top
-            }, 30);
-        });
-
-        scrollLinkUx.click(function(e) {
-            e.preventDefault();
-            $("html, body").animate({
-                scrollTop: $(this.hash).offset().top
-            }, 30);
-        });
-
-        // quitar el url #
-        $('a[href*="#"]:not([href="#"])').on("click", function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-                if (target.length) {
-                    $("html, body").animate({
-                        scrollTop: target.offset().top
-                    }, 30);
-                    return false;
-                }
-            }
-        });
-    }
-
-    $(".hide_modal").on("click", function() {
-        $("body").removeClass("page-category");
-        $(".modal_cont").removeClass("modal_cont_animate");
-    });
-
-    $(".show_modal1").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog1").addClass("modal_cont_animate");
-    });
-
-    $(".show_modal2").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog2").addClass("modal_cont_animate");
-    });
-
-    $(".show_modal3").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog3").addClass("modal_cont_animate");
-    });
-
-    setTimeout(function() {
-        $(".loader_bg").fadeToggle();
-    }, 1760);
+	$(".js_popup-hide").on("click", function() {
+		$(".popup__login").removeClass("popup_animation");
+		$(".page-body").css({"overflow": "auto"});
+	});
 });
